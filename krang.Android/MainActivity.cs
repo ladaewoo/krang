c35dad6c9b4d06ab502d517d46477c98fd1cf0d2
ladaewoo <ladaewoo@hotmail.com>
2021-12-05 07:53:06 -0600

    first commit

---
+++ krang.Android/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+
+namespace krang.Droid
+{
+    [Activity(Label = "Calculadora Básica", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 255, 0, 0));
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ krang/MainPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace krang
+{
+    public partial class MainPage : ContentPage
+    {
+        public string operando_actual = "";
+        public float operando_anterior = 0;
+        public string operador = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Reset(System.Object sender, System.EventArgs e)
+        {
+            OperandoLabel.Text = "0";
+            operando_actual = "";
+            operando_anterior = 0;
+        }
+
+        void Concatenar(System.Object sender, System.EventArgs e)
+        {
+            string valor_actual = ((Button)sender).Text;
+            if (operando_actual == "0" || valor_actual == "-") operando_actual = "";
+            OperandoLabel.Text = operando_actual;
+            if (valor_actual == "0" && operando_actual == "0") return;
+            operando_actual += valor_actual;
+            Console.WriteLine(operando_actual);
+            OperandoLabel.Text = operando_actual;
+        }
+
+        void AgregarPunto(System.Object sender, System.EventArgs e)
+        {
+            
+
+            if(operando_actual.Length > 0)
+            {
+                Match m = Regex.Match(operando_actual, @"[.]+");
+
+                if (!m.Success)
+                {
+                    operando_actual += ".";
+                }
+            } else
+            {
+                operando_actual = "0.";
+            }
+
+            OperandoLabel.Text = operando_actual;
+        }
+
+        void InvertirPrefijo(System.Object sender, System.EventArgs e)
+        {
+            if(!Double.IsNaN(Convert.ToDouble(operando_actual)))
+            {
+                float valorInvertido = float.Parse(operando_actual) * -1;
+                operando_actual = valorInvertido.ToString();
+                OperandoLabel.Text = operando_actual;
+            }
+            
+        }
+
+        void Retroceder(System.Object sender, System.EventArgs e)
+        {
+            operando_actual = (operando_actual.Length <= 1) ? "0" : operando_actual.Substring(0, operando_actual.Length - 1);
+            OperandoLabel.Text = operando_actual;
+        }
+
+        void ObtenerResultado(System.Object sender, System.EventArgs e)
+        {
+            if(operador != null)
+            {
+                float resultado = Calcular(operando_anterior, operando_actual.Length > 0 ? float.Parse(operando_actual) : 0, operador);
+
+                if(Double.IsInfinity((double)resultado))
+                {
+                    resultado = 0;
+                }
+                operando_actual = resultado.ToString();
+                OperandoLabel.Text = operando_actual;
+            }
+            else
+            {
+                OperandoLabel.Text = operando_actual.Length > 0 ? operando_actual : "0";
+            }
+            
+            operador = null;
+        }
+
+        void Operar(System.Object sender, System.EventArgs e)
+        {
+            if (operador == null)
+            {
+                
+                operando_anterior = operando_actual.Length > 0 ? float.Parse(operando_actual) : 0;
+            }
+            else
+            {
+                operando_anterior = Calcular(operando_anterior, operando_actual.Length > 0 ? float.Parse(operando_actual) : 0, operador);
+            }
+
+            operando_actual = "";
+            operador = ((Button)sender).Text;
+            OperandoLabel.Text = operando_anterior.ToString();
+        }
+
+        float Calcular(float a, float b, string operador)
+        {
+            float resultado = 0;
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = a + b;
+                    break;
+                case "-":
+                    Console.WriteLine(a.ToString(), b.ToString(), (a - b).ToString());
+                    resultado = a - b;
+                    break;
+                case "*":
+                    resultado = a * b;
+                    break;
+                case "÷":
+                    resultado = a / b;
+                    break;
+            }
+
+            return resultado;
+        }
+    }
+}
